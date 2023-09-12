32688a471198f8034c84ccecb3e180b48db159f8
Matheus Magno <matheusmagnon@gmail.com>
2023-09-11 23:55:29 -0300

    storybook: add Headers and Orders components

---
+++ src/components/Header.stories.tsx
@@ -0,0 +1,10 @@
+import { Header } from './Header'
+import { Meta, StoryObj } from '@storybook/react'
+
+export default {
+    title: 'Components/Header',
+    component: Header
+
+} as Meta
+
+export const Default: StoryObj = {}
