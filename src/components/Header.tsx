a6773d710bd53790e0b951cc5be6d693e5302a53
Matheus Magno <matheusmagnon@gmail.com>
2023-09-12 23:35:09 -0300

    uninstall: storybook

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@ import logo from '../../src/assets/images/logo.png'
 
 export function Header() {
     return (
-        <div className="w-full flex flex-col py-4 items-center">
+        <header className="w-full flex flex-col py-4 items-center">
             <img src={logo} className='w-72'>
             </img>
-        </div >
+        </header >
     )
 }
